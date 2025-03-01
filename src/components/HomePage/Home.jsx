81ebe57c579e884789b64d13aa9e52f9a2a8d169
Shravanis30 <salunkeshravani561@gmail.com>
2025-03-01 12:54:37 +0530

    added components

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -11,6 +11,36 @@ const Home = () => {
         <Link to="/schedule">
           <button className="px-4 py-2 bg-green-500 text-white rounded">Student Dashboard</button>
         </Link>
+        <Link to="/dailyYogaChallenge">
+          <button className="px-4 py-2 bg-green-500 text-white rounded">dailyYogaChallenge</button>
+        </Link>
+        <Link to="/yogaMeditation">
+          <button className="px-4 py-2 bg-green-500 text-white rounded">yogaMeditation</button>
+        </Link>
+        <Link to="/mainStudentPage">
+          <button className="px-4 py-2 bg-green-500 text-white rounded">mainStudentPage</button>
+        </Link>
+        <Link to="/motivationalQuote">
+          <button className="px-4 py-2 bg-green-500 text-white rounded">motivationalQuote</button>
+        </Link>
+        <Link to="/parentLogin">
+          <button className="px-4 py-2 bg-green-500 text-white rounded">parentLogin</button>
+        </Link>
+        <Link to="/adminLogin">
+          <button className="px-4 py-2 bg-green-500 text-white rounded">adminLogin</button>
+        </Link> 
+        <Link to="/facultyLogin">
+          <button className="px-4 py-2 bg-green-500 text-white rounded">parentLogin</button>
+        </Link>
+        <Link to="/studentLogin">
+          <button className="px-4 py-2 bg-green-500 text-white rounded">studentLogin</button>
+        </Link>
+        <Link to="/roleSelection">
+          <button className="px-4 py-2 bg-green-500 text-white rounded">roleSelection</button>
+        </Link>
+        <Link to="/landingPage">
+          <button className="px-4 py-2 bg-green-500 text-white rounded">landingPage</button>
+        </Link>
       </div>
     </div>
   );
